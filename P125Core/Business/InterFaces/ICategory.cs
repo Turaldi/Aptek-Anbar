73f102c39e62b2058a52bb5ca5b3654f5dd91737
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-22 20:42:05 +0400

    Add files via upload

---
+++ P125Core/Business/InterFaces/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.InterFaces
+{
+    public interface ICategory
+    {
+        Drug_Category Create(Drug_Category drug_Category);
+        Drug_Category Update(int Id, Drug_Category drug_Category);
+        Drug_Category Delete(int Id);
+        Drug_Category Get(int Id);
+        Drug_Category Get(string name);
+        List<Drug_Category> GetAll();
+        List<Drug_Category> GetAll(int Category);
+    }
+}

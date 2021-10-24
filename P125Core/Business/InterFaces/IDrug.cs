52531d8a0c01c61e9759a8e9369d3d332ce199c6
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-24 23:31:41 +0400

    IDrug.cs
    Add interface Drug

---
+++ P125Core/Business/InterFaces/IDrug.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.InterFaces
+{
+    public  interface IDrug
+    {
+        Drug Create(Drug drug, string categoryName);
+        Drug Delete(int Id);
+        Drug Update(Drug drug, string categoryName);
+        List<Drug> Get(string name);
+        List<Drug> GetAll(string categoryName);
+        List<Drug> GetAll();
+
+    }
+}

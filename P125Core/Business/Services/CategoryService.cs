8c04a4a896fe15f3c05e1da1a682c4e97fc3bffa
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-24 23:32:20 +0400

    Service
    Add services drug and category

---
+++ P125Core/Business/Services/CategoryService.cs
@@ -0,0 +1,81 @@
+﻿using Business.InterFaces;
+using DataAccess.Repositories;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Services
+{
+    public class CategoryService : ICategory
+    {
+        public CategoryRepository categoryRepository { get; set; }
+        private static int count  { get; set; }
+        public CategoryService()
+        {
+            categoryRepository = new CategoryRepository();
+        }
+        public Drug_Category Create(Drug_Category drug_Category)
+        {
+            try
+            {
+                drug_Category.Id = count;
+                Drug_Category isExist = categoryRepository.Get(g => g.Name.ToLower() == drug_Category.Name.ToLower());
+                if (isExist != null)
+                    return null;
+                categoryRepository.Create(drug_Category);
+                count++;
+                return drug_Category;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public Drug_Category Delete(int Id)
+        {
+            Drug_Category dbCategory = categoryRepository.Get(g => g.Id == Id);
+            if (dbCategory!=null)
+            {
+                categoryRepository.Delete(dbCategory);
+                return dbCategory;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Drug_Category Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drug_Category Get(string name)
+        {
+            return categoryRepository.Get(g => g.Name.ToLower() == name.ToLower());
+        }
+
+        public List<Drug_Category> GetAll()
+        {
+            return categoryRepository.GetAll(); 
+        }
+
+        public List<Drug_Category> GetAll(int Category)
+        {
+            return categoryRepository.GetAll(g => g.Category == Category);
+        }
+
+        public Drug_Category Update(int Id, Drug_Category drug_Category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ P125Core/Business/Services/DrugService.cs
@@ -0,0 +1,71 @@
+﻿using Business.InterFaces;
+using DataAccess.Repositories;
+using Entities.Models;
+using System;
+using System.Collections.Generic; 
+using System.Text;
+
+namespace Business.Services
+{
+    public class DrugService : IDrug
+    {
+        private DrugRepository drugRepository { get;  }
+        public CategoryService categoryService { get; }
+        private static int count;
+        public DrugService()
+        {
+            drugRepository = new DrugRepository();
+            categoryService = new CategoryService();
+        }
+        public Drug Create(Drug drug, string categoryName)
+        {
+            Drug_Category dbCategory = categoryService.Get(categoryName);
+            if (dbCategory !=null)
+            {
+                drug.Drug_Category = dbCategory;
+                drug.Id = count;
+                drugRepository.Create(drug);
+                count++;
+                return drug;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Drug Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drug> Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Drug> GetAll(string categoryName)
+        {
+            Drug_Category dbCategory = categoryService.Get(categoryName);
+            if (dbCategory!=null)
+            {
+                return drugRepository.GetAll(s => s.Drug_Category.Name == dbCategory.Name);
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Drug> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Drug Update(Drug drug, string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

70dd9ae8f5bc014251a32902a37d1bd3e18a0bbd
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-24 23:33:14 +0400

    Controllers
    Add controllers drug and category

---
+++ P125Core/CodeAkademyApp/Controllers/CategoryControllers.cs
@@ -0,0 +1,101 @@
+﻿using Business.Services;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utilies.Helpers;
+
+namespace CodeAkademyApp.Controllers
+{
+    public class CategoryControllers
+    {
+        public CategoryService categoryService { get; set; }
+
+        public CategoryControllers()
+        {
+            categoryService = new CategoryService();
+        }
+        public void Create()
+        {
+            Helper.ChangeTextColor(ConsoleColor.Cyan, "Enter Drug Name");
+            string name = Console.ReadLine();
+        EnterName: Helper.ChangeTextColor(ConsoleColor.Cyan, "Enter Drug Category Name");
+            string category = Console.ReadLine();
+            int Category;
+            bool isTrueCategory = int.TryParse(category, out Category);
+            if (isTrueCategory)
+            {
+                Drug_Category _Category = new Drug_Category { Name = name, Category = Category };
+                if (categoryService.Create(_Category) != null)
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Green, $"{_Category.Name} created");
+                    return;
+                }
+                else
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Green, "Something is wrong!");
+                    return;
+                }
+            }
+            else
+            {
+                Helper.ChangeTextColor(ConsoleColor.Red, "Enter correct Category");
+                goto EnterName;
+            }
+        }
+
+        public void GetAllCategories()
+        {
+            Helper.ChangeTextColor(ConsoleColor.Yellow, "All categories:");
+            foreach (Drug_Category _Category in categoryService.GetAll())
+            {
+                Helper.ChangeTextColor(ConsoleColor.Blue, $"{_Category.Id}-{_Category.Name}");
+            }
+        }
+
+        public void Delete()
+        {
+            GetAllCategories();
+            Helper.ChangeTextColor(ConsoleColor.Yellow, "Enter categories Id:");
+            string input = Console.ReadLine();
+            int categoryId;
+            bool isTrue = int.TryParse(input, out categoryId);
+            if (isTrue)
+            {
+                if (categoryService.Delete(categoryId)!=null)
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Green, "Categories is deleted");
+                    return;
+                }
+                else
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Red, $"{categoryId} is not find");
+                    return;  
+                }
+            }
+            else
+            {
+                Helper.ChangeTextColor(ConsoleColor.Red, $"Please, select correct format");
+            }
+        }
+
+        public void GetCategorieswithdose()
+        {
+            Helper.ChangeTextColor(ConsoleColor.Yellow, "Enter category dose");
+            string input = Console.ReadLine();
+            int maxDose;
+            bool isTrue = int.TryParse(input, out maxDose);
+            if (isTrue)
+            {
+                Helper.ChangeTextColor(ConsoleColor.Blue, $"Categories which dose is {maxDose}");
+                foreach (var item in categoryService.GetAll(maxDose))
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Cyan, item.Name);
+                }
+                return;
+            }
+            Helper.ChangeTextColor(ConsoleColor.Red, $"Please,select correct format");
+
+        }
+    }
+}
---
+++ P125Core/CodeAkademyApp/Controllers/DrugControllers.cs
@@ -0,0 +1,55 @@
+﻿using Business.Services;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utilies.Helpers;
+
+namespace CodeAkademyApp.Controllers
+{
+    public  class DrugControllers
+    {
+        private DrugService drugService { get; }
+        public CategoryService categoryService { get; set; }
+        public DrugControllers()
+        {
+            drugService = new DrugService();
+            categoryService = new CategoryService();
+        }
+
+        public void Create()
+        {
+            Helper.ChangeTextColor(ConsoleColor.Blue, "Select possible category:");
+            string categoryName = Console.ReadLine();
+            Helper.ChangeTextColor(ConsoleColor.Blue, "Enter drug name");
+            string name = Console.ReadLine();
+            Drug drug = new Drug { Name = name };
+            Drug newDrug = drugService.Create(drug, categoryName);
+            if (newDrug!=null)
+            {
+                Helper.ChangeTextColor(ConsoleColor.Green, $"New drug created -{newDrug.Name}");
+                return;
+            }
+            Helper.ChangeTextColor(ConsoleColor.Red, $"Couldn't find such as category-{categoryName}");
+
+        }
+
+        public void GetAllDrugwithCategory()
+        {
+            Helper.ChangeTextColor(ConsoleColor.Blue, "Select possible category");
+            string categoryName = Console.ReadLine();
+            List<Drug> drugs = drugService.GetAll(categoryName);
+            if (drugs!=null)
+            {
+                Helper.ChangeTextColor(ConsoleColor.Blue, $"Category {categoryName}");
+                foreach (var item in drugs)
+                {
+                    Helper.ChangeTextColor(ConsoleColor.Green, $"{item.Id}-{item.Name}");
+
+                }
+                return;
+            }
+            Helper.ChangeTextColor(ConsoleColor.Red, $"Couldn't find such as category-{categoryName}");
+        }
+    }
+}

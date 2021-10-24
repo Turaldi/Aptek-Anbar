737efb1fd0fd2549bad394c94b5397ce2a866205
Turaldi <91114292+Turaldi@users.noreply.github.com>
2021-10-24 23:34:04 +0400

    Repository
    Drug and category Repository

---
+++ P125Core/DataAccess/InterFaces/Repositories/CategoryRepository.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.InterFaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    public class CategoryRepository : IRepository<Drug_Category>
+    {
+        public bool Create(Drug_Category entitiy)
+        {
+            try
+            {
+                DbContext.Drug_Categories.Add(entitiy);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Drug_Category entitiy)
+        {
+            try
+            {
+                DbContext.Drug_Categories.Remove(entitiy);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Drug_Category Get(Predicate<Drug_Category> filter = null)
+        {
+            try
+            {
+                return filter == null ? DbContext.Drug_Categories[0]
+                    : DbContext.Drug_Categories.Find(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Drug_Category> GetAll(Predicate<Drug_Category> filter = null)
+        {
+            try
+            {
+                return filter == null ? DbContext.Drug_Categories
+                    : DbContext.Drug_Categories.FindAll(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(Drug_Category entitiy)
+        {
+            try
+            {
+                Drug_Category Drug_Category = Get(s => s.Id == entitiy.Id);
+                Drug_Category.Name = entitiy.Name;
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+
+}
---
+++ P125Core/DataAccess/InterFaces/Repositories/DrugRepository.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.InterFaces;
+using Entities.InterFace;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Repositories
+{
+    public class DrugRepository : IRepository<Drug>
+
+    {
+        public bool Create(Drug entitiy)
+        {
+            try
+            {
+                DbContext.Drugs.Add(entitiy);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Drug entitiy)
+        {
+            try
+            {
+                DbContext.Drugs.Remove(entitiy);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Drug Get(Predicate<Drug> filter = null)
+        {
+            try
+            {
+                return filter == null ? DbContext.Drugs[0]
+                    : DbContext.Drugs.Find(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Drug> GetAll(Predicate<Drug> filter = null)
+        {
+            try
+            {
+                return filter == null ? DbContext.Drugs
+                    : DbContext.Drugs.FindAll(filter);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Update(Drug entitiy)
+        {
+            try
+            {
+                Drug dbDrug = Get(s => s.Id == entitiy.Id);
+                dbDrug = entitiy;
+                return true;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
